749e21987afe0b07891c5218b90e89ba87e34d1c
Leonhard Markert <curiousleo@ymail.com>
2015-08-11 11:09:32 +0200

    src/Makefile: create Everything and check it

--- bin/everything.sh
+++ bin/everything.sh
@@ -1,8 +1,5 @@
-echo "module Everything where" > Everything.agda
-echo >> Everything.agda
-
 for f in `find -type f -name '*.agda' | sed 's#^\./\(.*\)\.agda#\1#' | sed 's#/#\.#g'`; do
   if [ "$f" != "Everything" ]; then
-    echo "open import $f" >> Everything.agda
+    echo "open import $f"
   fi
 done
